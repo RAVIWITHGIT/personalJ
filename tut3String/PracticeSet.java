36c6b3e12a423a546e06ebf3981354ebd0e87dc5
RAVIWITHGIT <ravilakshakar336@gmail.com>
2024-11-19 07:08:04 +0530

    update code

---
+++ tut3String/PracticeSet.java
@@ -0,0 +1,25 @@
+public class PracticeSet{
+    public static void main(String args[]){
+        // String name = "Ravi Lakshakar";
+
+        // name = name.toLowerCase();
+        // System.out.println(name);
+
+        // name = name.replace(" ","_");
+        // System.out.println(name);
+
+        // String name2 = "Dear <|name|>, Thanks a lot";
+        // name2 = name2.replace("<|name|>","ravi");
+        // System.out.println(name2);
+
+        // String name2 = "Dear <|name|>,  Thanks     a lot";
+        // System.out.println(name2.indexOf("  "));
+        // System.out.println(name2.indexOf("   "));
+
+        String name2 = "dear sir \n\ti want \"one day leave\" \n\tThanks lot";
+        System.out.println(name2);
+
+
+
+    }
+}
