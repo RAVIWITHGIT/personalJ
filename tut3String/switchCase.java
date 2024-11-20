69c65b4ede268c38abd78fdd52b3b35ed2704833
ProjectWithRavi <raviyt73@gmail.com>
2024-11-20 06:36:44 +0530

    update code

---
+++ tut3String/switchCase.java
@@ -0,0 +1,55 @@
+public class switchCase{
+    public static void main(String args[]){
+        // Character ch = 't';
+        // switch(ch){
+        //     case 'r':
+        //         System.out.println("your name start with r character");
+        //         break;
+        //     case 'a':
+        //         System.out.println("your name start with a character");
+        //         break;
+        //     case 'p':
+        //         System.out.println("your name start with p character");
+        //         break;
+        //     case 'm':
+        //         System.out.println("your name start with m character");
+        //         break;
+        //     default:
+        //         System.out.println("no character match in case");    
+        // }
+
+
+        // ******** second way to create switch case
+        // Character ch = 'r';
+        // switch(ch){
+        //     case 'r' -> System.out.println("your name start with r character");
+        //     case 'a' -> System.out.println("your name start with r character");
+        //     case 'm' -> System.out.println("your name start with r character");
+        //     case 'p' -> System.out.println("your name start with r character");
+        //     default -> System.out.println("no character match in case");    
+        // }
+
+        // ******** second way to create switch case with multiple line print
+        // Character ch = 'r';
+        // switch(ch){
+        //     case 'r' -> {
+        //         System.out.println("your name start with r character");
+        //         System.out.println("is your name ravi");
+        //         }
+        //     case 'a' -> {
+        //         System.out.println("your name start with a character");
+        //         System.out.println("is your name aakash");
+        //         }
+        //     case 'p' -> {
+        //         System.out.println("your name start with p character");
+        //         System.out.println("is your name parmod");
+        //         }
+        //     case 's' -> {
+        //         System.out.println("your name start with s character");
+        //         System.out.println("is your name shiv");
+        //         }
+           
+        //     default -> System.out.println("no character match in case");    
+        // }
+    }
+}
