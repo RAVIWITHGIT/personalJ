6bac2667442854333af8317940f53b681230b278
ProjectWithRavi <raviyt73@gmail.com>
2024-11-23 06:54:29 +0530

    update code

--- tut6Arrays/Arrays.java
+++ tut6Arrays/Arrays.java
@@ -9,9 +9,19 @@ second --> int myArr[];
 
  */
 
+public class Arrays {
+    public static void main(String args[]) {
+        float[] myArr = { 1.2f, 3.5f, 8.4f, 7.8f, 6.5f };
 
-public class Arrays{
-    public static void main(String args[]){
-        
+        // traverse array element using for loop
+        for (int i = 0; i < myArr.length; i++) {
+            System.out.println(myArr[i]);
+        }
+
+        // traverse array element using forEach loop
+        // for(float element : myArr){
+        // System.out.println(element);
+        // }
+
     }
 }
