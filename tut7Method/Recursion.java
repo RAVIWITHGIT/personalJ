d22e4d04da31989471b0fd4158e80c3b0e93f8d9
RAVIWITHGIT <ravilakshakar336@gmail.com>
2024-11-24 14:14:41 +0530

    update code

---
+++ tut7Method/Recursion.java
@@ -0,0 +1,61 @@
+public class Recursion {
+
+    // ***************** this function work 5*4*3*2*1
+    // static int factorial(int n) {
+    // if (n == 0 || n == 1) {
+    // return 1;
+    // } else {
+    // return n * factorial(n - 1);
+    // }
+
+    // }
+
+    // static int factorialWithAscending(int n, int current) {
+    // if (current > n) {
+    // return 1;
+    // } else {
+    // return current * factorialWithAscending(n, current + 1);
+    // }
+
+    // }
+
+    // static void printNumber(int n) {
+    // if (n == 1) {
+    // System.out.println(1);
+    // } else {
+    // printNumber(n - 1);
+    // System.out.println(n);
+    // }
+
+    // }
+
+    // static void fibonacciSeries(int first, int second, int range) {
+    // if (range < second) {
+
+    // } else if (first == 0 && second == 1) {
+    // System.out.println(first);
+    // System.out.println(second);
+    // int temp = first + second;
+    // first = second;
+    // second = temp;
+    // fibonacciSeries(first, second, range);
+    // } else {
+    // System.out.println(second);
+    // int temp = first + second;
+    // first = second;
+    // second = temp;
+    // fibonacciSeries(first, second, range);
+    // }
+
+    // }
+
+    public static void main(String args[]) {
+        // int result = factorial(5);
+        // System.out.println(result);
+        // int result = factorialWithAscending(5, 1);
+        // System.out.println(result);
+        // printNumber(5);
+        // System.out.println(result);
+        // fibonacciSeries(0, 1, 50);
+    }
+}
