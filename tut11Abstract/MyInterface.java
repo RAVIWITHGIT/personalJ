3907ad44651e505298a7b6d9db2230596a4f334b
RAVIWITHGIT <ravilakshakar336@gmail.com>
2024-12-01 12:46:36 +0530

    update code

---
+++ tut11Abstract/MyInterface.java
@@ -0,0 +1,48 @@
+/* ***************************************** interface *********************************************
+1.in java interface is a group of related methods with empty bodies
+2. in abstract we can only  expend one abstract class ,but interface we can use multiple interface in sub class 
+3. it compulsory that use  all interface method in subClass
+4. we can not modify interface properties in subclass 
+5. when use interface method in sub class then make method as public
+5. more read in future
+ */
+
+// package tut11Abstract;
+
+interface Bicycle {
+    int speed = 30;
+
+    void applyBreak();
+
+    void speedUp();
+
+}
+
+interface Electrical {
+    int eSpeed = 50;
+
+    void lightColor(String color);
+
+}
+
+class Modified implements Bicycle, Electrical {
+    public void applyBreak() {
+        System.out.println("apply break");
+    }
+
+    public void speedUp() {
+        System.out.println("increase Modified speed ");
+    }
+
+    public void lightColor(String color) {
+        System.err.printf("on %s light", color);
+    }
+}
+
+public class MyInterface {
+    public static void main(String args[]) {
+        Modified mix = new Modified();
+        mix.applyBreak();
+        mix.lightColor("red");
+    }
+}
