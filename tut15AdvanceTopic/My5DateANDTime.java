a875abdc3f1f3f4a7edfeacc08682cf49fb04278
RAVIWITHGIT <ravilakshakar336@gmail.com>
2024-12-11 06:33:05 +0530

    update code

--- tut15AdvanceTopic/My5DateANDTime.java
+++ tut15AdvanceTopic/My5DateANDTime.java
@@ -1,4 +1,7 @@
-import java.util.Date;
+// import java.util.Calendar;
+// import java.util.GregorianCalendar;
+// import java.util.Date;
+// import java.util.TimeZone;
 
 public class My5DateANDTime {
     public static void main(String args[]) {
@@ -16,7 +19,38 @@ public class My5DateANDTime {
         // System.out.println(v);
 
         // *********************************
-        Date d = new Date();
-        System.out.println(d);
+        // Date d = new Date();
+        // System.out.println(d);
+
+        // Date d2 = new Date(1733875686654L);
+        // System.out.println(d2);
+
+        // ************************** Calendar *************************
+        // Calendar c = Calendar.getInstance();
+        // System.out.println(c);
+        // System.out.println(c.getCalendarType());
+        // System.out.println(c.getTimeZone());
+
+        // ** manually set time zone
+        // Calendar c =
+        // Calendar.getInstance(TimeZone.getTimeZone("America/Los_Angeles"));
+        // System.out.println(c.getCalendarType());
+        // System.out.println(c.getTimeZone());
+        // System.out.println(c.getTime());
+        // System.out.println(c.get(Calendar.YEAR));
+        // System.out.println(c.getTime());
+        // c.add(Calendar.DATE, -15);
+        // System.out.println("15 day ago " + c.getTime());
+
+        // *************************** GregorianCalendar *******************************
+        // GregorianCalendar g = new GregorianCalendar();
+        // System.out.println(g.isLeapYear(2024));
+
+        // ***************************** TimeZone *****************************
+        // System.out.println(TimeZone.getAvailableIDs()[0]);
+        // for (String value : TimeZone.getAvailableIDs()) {
+        // System.out.println(value);
+        // }
+
     }
 }
