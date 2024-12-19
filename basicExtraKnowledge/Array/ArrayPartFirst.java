974d53cd40d51399acb2888ea52a96b4178976f3
RAVIWITHGIT <ravilakshakar336@gmail.com>
2024-12-19 07:26:22 +0530

    update code

---
+++ basicExtraKnowledge/Array/ArrayPartFirst.java
@@ -0,0 +1,26 @@
+package basicExtraKnowledge.Array;
+
+public class ArrayPartFirst {
+    public static void main(String args[]) {
+        int number[] = { 1, 2, 3, 4, 5 };
+        System.out.println(number); /*
+                                     * output ==> [I@1372ed45 , [ mean array and I mean Integer and all rest value
+                                     * (@1372ed45) is random value
+                                     */
+
+        double mydoble[] = { 2.34, 5.66 };
+        System.out.println(mydoble); /*
+                                      * output ==> [D@797badd3 , [ mean array and D mean double and all rest value
+                                      * (@797badd3) is random value
+                                      */
+
+        float myfloat[] = { 2.34f, 5.66f };
+        System.out.println(myfloat); /*
+                                      * output ==> [F@3f200884 , [ mean array and F mean float and all rest value
+                                      * (@3f200884) is random value
+                                      */
+
+        // foreach and enhances for loop . this is same but different pronunciation
+
+    }
+}
